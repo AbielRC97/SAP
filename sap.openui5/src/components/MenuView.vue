021d56efeaf4c6f849849779c709b66a6c428bb4
ia.rosales <ia.rosales@mavi.mx>
2022-09-28 13:36:31 -0500

    MenuView

---
+++ sap.openui5/src/components/MenuView.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+nav.navbar.navbar-expand-lg.navbar-light.bg-light
+  a.navbar-brand(href="#") Navbar
+  button.navbar-toggler(
+    type="button",
+    data-toggle="collapse",
+    data-target="#navbarNav",
+    aria-controls="navbarNav",
+    aria-expanded="false",
+    aria-label="Toggle navigation"
+  )
+    span.navbar-toggler-icon
+  #navbarNav.collapse.navbar-collapse
+    ul.navbar-nav
+      li.nav-item.active
+        router-link.nav-link(to="/")
+          | Contador
+      li.nav-item
+        router-link.nav-link(to="/avatar") Avatar
+      li.nav-item
+        a.nav-link(href="#") Pricing
+      li.nav-item
+        a.nav-link.disabled(href="#") Disabled
+</template>
