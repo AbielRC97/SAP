2246a9818b3a8221ee956767920ebfa3d734596a
ia.rosales <ia.rosales@mavi.mx>
2022-09-28 11:30:37 -0500

    OpenUI5

--- sap.openui5/src/main.ts
+++ sap.openui5/src/main.ts
@@ -4,4 +4,9 @@ import router from "./router";
 import BootstrapVue from "bootstrap-vue-3";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-createApp(App).use(BootstrapVue).use(router).mount("#app");
+// createApp(App).use(BootstrapVue).use(router).mount("#app");
+const app = createApp(App);
+app.config.isCustomElement = (tag) => tag.startsWith("ui5-");
+app.use(BootstrapVue);
+app.use(router);
+app.mount("#app");
---
+++ sap.openui5/src/views/ControlsView.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+.container: .row
+  .col-md-12: span.text-success Hola Mundo
+  .col-md-12: ui5-button Hello world !!
+</template>
+<script lang="ts" setup>
+import "@ui5/webcomponents/dist/Button.js";
+import { onMounted, nextTick } from "vue";
+onMounted(async () => {
+  await nextTick();
+});
+</script>
