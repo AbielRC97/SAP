3f54dfa71486da107254f89f09006917ac143c7f
ia.rosales <ia.rosales@mavi.mx>
2022-09-28 12:16:40 -0500

    Ejemplo Contador

--- sap.openui5/src/router/index.ts
+++ sap.openui5/src/router/index.ts
@@ -4,17 +4,6 @@ import HomeView from "../views/HomeView.vue";
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-  {
-    path: "/sap",
     name: "sap",
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/ControlsView.vue"),
