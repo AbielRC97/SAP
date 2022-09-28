68ac28db8e5c9e877e50edb30f114a16cd828a29
ia.rosales <ia.rosales@mavi.mx>
2022-09-28 10:59:21 -0500

    Implementacion de Bootstrap

--- sap.openui5/src/main.ts
+++ sap.openui5/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import BootstrapVue from "bootstrap-vue-3";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(BootstrapVue).use(router).mount("#app");
