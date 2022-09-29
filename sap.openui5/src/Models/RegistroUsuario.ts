aa2be307b3c15cced663cfe8a3e7aa4c70daa15a
ia.rosales <ia.rosales@mavi.mx>
2022-09-29 17:13:29 -0500

    Modelo Registro de Usuario

---
+++ sap.openui5/src/Models/RegistroUsuario.ts
@@ -0,0 +1,5 @@
+export interface RegistroUsuario {
+  nickname?: string;
+  email?: string;
+  contrasena?: string;
+}
--- sap.openui5/src/views/RegistroView.vue
+++ sap.openui5/src/views/RegistroView.vue
@@ -1,10 +1,20 @@
 <template lang="pug">
 .container: .row
-  .col-sm-12.col-md-12.col-lg-12.col-xl-12.col-xxl-12: span.display-4 Registrar Usuario
+  .col-sm-12.col-md-12.col-lg-12.col-xl-12.col-xxl-12.mt-4: .card: .card-body: .row
+    .col-sm-12.col-md-12.col-lg-12.col-xl-12.col-xxl-12.mt-2: p.fw-bold.h2 Registrar Usuario
+    .col-sm-12.col-md-12.col-lg-12.col-xl-12.col-xxl-12.mt-2: ui5-input.form-control(
+      value="Input",
+      placeholder="Registrar Usuario"
+    )
 </template>
 <script lang="ts" setup>
-import { onMounted, nextTick } from "vue";
+import "@ui5/webcomponents/dist/Input.js";
+import "@ui5/webcomponents/dist/features/InputSuggestions.js";
+import { onMounted, nextTick, ref } from "vue";
+import { RegistroUsuario } from "../Models/RegistroUsuario";
+const usuario = ref({} as null | RegistroUsuario);
 onMounted(async () => {
+  usuario.value = null;
   await nextTick();
 });
 </script>
