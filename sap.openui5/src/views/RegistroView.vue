03f2276df33cc9d628a2ef04de9ef3b941da4642
ia.rosales <ia.rosales@mavi.mx>
2022-09-29 16:34:58 -0500

    Vista Registro de Usuario

---
+++ sap.openui5/src/views/RegistroView.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+.container: .row
+  .col-sm-12.col-md-12.col-lg-12.col-xl-12.col-xxl-12: span.display-4 Registrar Usuario
+</template>
+<script lang="ts" setup>
+import { onMounted, nextTick } from "vue";
+onMounted(async () => {
+  await nextTick();
+});
+</script>
