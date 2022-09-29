1753b5aa3956f583ad75e3269712d300f952f2f8
ia.rosales <ia.rosales@mavi.mx>
2022-09-29 13:35:22 -0500

    Evento Cambio

---
+++ sap.openui5/src/Tools/Utilities.ts
@@ -0,0 +1,8 @@
+export enum Tipo {
+  Slash = "Slash",
+  BackSlash = "BackSlash",
+  DoubleBackSlash = "DoubleBackSlash",
+  DoubleGreaterThan = "DoubleGreaterThan",
+  DoubleSlash = "DoubleSlash",
+  GreaterThan = "GreaterThan",
+}
--- sap.openui5/src/views/BreadCrumbsView.vue
+++ sap.openui5/src/views/BreadCrumbsView.vue
@@ -4,10 +4,18 @@
     ui5-breadcrumbs-item(href="https://www.sap.com", target="_blank") Root Page
     ui5-breadcrumbs-item(href="https://www.sap.com") Parent Page
     ui5-breadcrumbs-item Current Page
-  .col-md-12: ui5-breadcrumbs(separator-style="BackSlash")
+  .col-md-12: ui5-breadcrumbs(:separator-style="tipo")
     ui5-breadcrumbs-item(href="https://www.sap.com", target="_blank") Root Page
     ui5-breadcrumbs-item(href="https://www.sap.com") Parent Page
     ui5-breadcrumbs-item Current Page
+  .col-md-12: ui5-combobox(
+    placeholder="Enter value",
+    :value="tipo",
+    v-model="tipo",
+    @change="cambiarDatos($event.target.value)"
+  )
+    ui5-cb-item(v-for="(item, index) in Tipos", :key="index", :text="item")
+  .col-md-12: span.text-success {{ tipo }}
 </template>
 <script lang="ts" setup>
 import "@ui5/webcomponents/dist/Button.js";
@@ -16,8 +24,24 @@ import "@ui5/webcomponents/dist/Avatar.js";
 import "@ui5/webcomponents-icons/dist/AllIcons.js";
 import "@ui5/webcomponents/dist/Breadcrumbs.js";
 import "@ui5/webcomponents/dist/BreadcrumbsItem.js";
-import { onMounted, nextTick } from "vue";
-onMounted(async () => {
+import "@ui5/webcomponents/dist/ComboBox.js";
+import "@ui5/webcomponents/dist/ComboBoxItem.js";
+import { onMounted, nextTick, ref } from "vue";
+import { Tipo } from "../Tools/Utilities";
+const Tipos = ref([] as Tipo[]);
+const tipo = ref({} as Tipo);
+async function cambiarDatos(value: any) {
+  console.log("I'm Horney");
+  console.log(value);
+  tipo.value = value as Tipo;
   await nextTick();
+}
+onMounted(async () => {
+  Tipos.value = Object.values(Tipo);
+  tipo.value = Tipo.Slash;
+  await nextTick(() => {
+    let Object = document.getElementById("ui5-combobox-input");
+    console.log(Object?.innerText);
+  });
 });
 </script>
