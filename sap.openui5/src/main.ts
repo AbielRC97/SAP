88a41f7ef6360bf80731b02f5ce023a97cf478d1
ia.rosales <ia.rosales@mavi.mx>
2022-10-03 13:07:34 -0500

    Vuex

--- sap.openui5/src/main.ts
+++ sap.openui5/src/main.ts
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import BootstrapVue from "bootstrap-vue-3";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 // createApp(App).use(BootstrapVue).use(router).mount("#app");
 const app = createApp(App);
 app.config.isCustomElement = (tag) => tag.startsWith("ui5-");
